8292c73b2f5463e61057536a20b65d78ba1d1358
jasminepw <jasminee.wonggg@gmail.com>
2022-11-14 21:39:48 -0800

    adding chart 1

---
+++ source/chart_1.R
@@ -0,0 +1,23 @@
+library(ggplot2)
+library(dplyr)
+
+data <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-group-7-BC/main/data/common-ocean-waste.csv")
+
+data <- data %>%
+  mutate(prop = round(Number_of_items / sum(data$Number_of_items) *100)) %>%
+  mutate(ypos = cumsum(prop) - 0.5*prop)
+
+chart <- ggplot(data, aes(x="", y = Number_of_items, fill = Characteristic)) +
+  geom_bar(stat = "identity", width = 0.5, color = 1) + 
+  coord_polar("y", start=0) +
+  geom_text(aes(label = paste0(prop, "%")),
+            position = position_stack(vjust = 0.5),
+            size = 3,
+            color = "white") +
+  labs(
+    title = "Most common items found during the International Coastal Ocean Cleanup in 2020",
+    caption = "Total waste items collected: 3,548,691",
+  ) +
+  theme_void()
+
+  
