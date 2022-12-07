ed880f5a61309a74fe145909189d35042cff313c
jasminepw <jasminee.wonggg@gmail.com>
2022-12-07 13:51:39 -0800

    tab chart 2

--- shiny/app_server.R
+++ shiny/app_server.R
@@ -10,7 +10,130 @@
 library(shiny)
 
 server <- function(input, output) {
-    # TBD
+# ----- [Tab 2 - Charts] ----- #
+  # Outputs a pie chart that answers RQ 3
+  output$piechart <- renderPlotly({
+    common_trash <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-group-7-BC/main/data/common-ocean-waste.csv")
+    
+    trash_pie <- common_trash %>%
+      mutate(prop = round(
+        Number_of_items / sum(common_trash$Number_of_items) * 100
+      )) %>%
+      rename(
+        Proportion = prop,
+        Type_of_trash = Characteristic
+      )
+    
+    pie1 <- plot_ly(
+      trash_pie,
+      labels = ~Type_of_trash,
+      values = ~Number_of_items,
+      type = "pie",
+      marker = list(colors = colors,
+                    line = list(color = '#FFFFFF', width = 1))
+    )
+    
+    pie1 <- pie1 %>%
+      layout(
+        title = "Most Common Items Found On Coastlines (2020)",
+        legend = list(title = list(text = "<b> Type of Trash </b>"))
+      )
+    
+    pie2 <- plot_ly(
+      trash_pie,
+      labels = ~Type_of_trash,
+      values = ~Number_of_items,
+      type = "pie",
+      textposition = 'inside',
+      textinfo = 'label+percent',
+      marker = list(colors = colors,
+                    line = list(color = '#FFFFFF', width = 1)),
+      showlegend = FALSE)
+    
+    pie2 <- pie2 %>%
+      layout(title = "Most Common Items Found On Coastlines (2020)")
+    
+    if(input$show == 1) {
+      return(pie1)
+    } else {
+      return(pie2)
+    }
+  })
+  
+  # Outputs a barplot that answers RQ 3
+  output$barplot <- renderPlotly({
+    countries_df <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-group-7-BC/main/data/plastic-pollution.csv")
+    
+    countries_df <- countries_df %>%
+      rename(
+        Mismanaged_Plastic = Mismanaged.plastic.waste.to.ocean.per.capita..kg.per.year.,
+        Country = Entity
+      )
+    
+    plot_data_max10 <- countries_df %>%
+      top_n(10, Mismanaged_Plastic)
+    
+    plot_data_max10$Country <- with(
+      plot_data_max10,
+      reorder(Country, Mismanaged_Plastic)
+    )
+    
+    plot_data_min10 <- countries_df %>%
+      filter(Mismanaged_Plastic != 0) %>%
+      top_n(-10, Mismanaged_Plastic)
+    
+    plot_data_min10$Country <- with(
+      plot_data_min10,
+      reorder(Country, -Mismanaged_Plastic)
+    )
+    
+    plot_input1 <- ggplot(
+      plot_data_max10,
+      aes(
+        x = Country,
+        y = Mismanaged_Plastic
+      )
+    ) +
+      geom_col(color = "black", fill = "red", alpha = 0.7) +
+      ggtitle(
+        "Top Ten Countries by Plastic Waste Emission to Ocean (2019)"
+      ) +
+      ylab("Plastic Waste per Capita (kg/year)") +
+      coord_flip()
+    
+    plot_input2 <- ggplot(
+      plot_data_min10,
+      aes(
+        x = Country,
+        y = Mismanaged_Plastic
+      )
+    ) +
+      geom_col(color = "black", fill = "red", alpha = 0.7) +
+      coord_flip() +
+      scale_y_continuous(labels = scales::comma) +
+      ggtitle(
+        "Countries with the Least Plastic Waste Emission to Ocean (2019)"
+      ) +
+      ylab("Plastic Waste per Capita (kg/year)")
+    
+    
+    if (input$max_min == 1) {
+      plot <- ggplotly(plot_input1)
+    } else {
+      plot <- ggplotly(plot_input2) %>%
+        layout(
+          margin = list(l = 50, r = 50, b = 100, t = 50),
+          annotations = list(
+            x = 1, y = -0.3, text =
+              "Note: Excludes countries with zero emissions.",
+            xref = "paper", yref = "paper", showarrow = F,
+            xanchor = "right", yanchor = "auto", xshift = 0, yshift = 0,
+            font = list(size = 10)
+          )
+        )
+    }
+    return(plot)
+  })
 }
 
 
--- shiny/tabs/tab_panel_chart2.R
+++ shiny/tabs/tab_panel_chart2.R
@@ -2,7 +2,104 @@
 
 library(shiny)
 
+show_pie_chart <- radioButtons(
+  inputId = "show",
+  label = h5("Display Piechart Aesthetic:"),
+  choices = list(
+    "Normal" = 1,
+    "Simple" = 2
+  )
+)
+
+show_max_min <- selectInput(
+  inputId = "max_min",
+  label = h5("Show Ten Countries with:"),
+  choices = list(
+    "the MOST Ocean Plastic Emissions" = 1,
+    "the LEAST Ocean Plastic Emissions" = 2
+  )
+)
+
 tab_panel_chart2 <-tabPanel(
-    "Chart 2",
-    p("This is chart 2.")
+  "Chart 2",
+  titlePanel(
+    "Interactive Charts: Common Trash & Countries' Plastic Waste Emissions"),
+  hr(),
+  h3("What are common waste items that end up on beaches and shorelines?"),
+  br(),
+  sidebarLayout(
+    position = "left",
+    sidebarPanel(
+      show_pie_chart
+    ),
+    mainPanel(
+      plotlyOutput("piechart"),
+      p(strong("Caption: "), "Hover information displays the type of trash, 
+        amount collected, and proportion percentage. ", strong("Note:"), "Other 
+        trash refers to items that do not fall in a specific category."),
+      h4(strong("Summary")),
+      p("This chart answers the third research question: “What are common waste 
+        items that end up on beaches and shorelines?” This chart shows the 
+        proportions of the most common items found on coastlines that were 
+        collected during the International Coastal Ocean Cleanup in 2020. 
+        There were 3,548,691 items collected. The most common item found was 
+        cigarette butts, with 27% of all the items collected. The least common 
+        item found was glass beverage bottles, with 4% of all the items 
+        collected."),
+      p("This data should be used to influence policymakers to implement better 
+        recycling and waste management practices at beaches and coastal 
+        recreation areas, as well as in general. For example, there should be 
+        specific smoking areas where people can utilize the space away from the 
+        general public to smoke and discard their cigarette butts. There should 
+        be trashcans, recycling, and compost bins distributed throughout the 
+        areas for people to dispose of their waste in the respective bin easily.
+        Policymakers should implement policies that will fine people who do not 
+        dispose of their waste properly.")
+    )
+  ),
+  hr(),
+  h3("What top ten countries emit the most and least plastic waste?"),
+  br(),
+  sidebarLayout(
+    position = "left",
+    sidebarPanel(
+      show_max_min
+    ),
+    mainPanel(
+      plotlyOutput("barplot"),
+      p(strong("Caption: "), "This chart shows the top ten countries by most and
+        least mismanaged plastic waste to the ocean per capita in kilograms per 
+        year."),
+      h4(strong("Summary")),
+      p("This chart answers the fourth research question: “What top ten 
+        countries emit the most and least plastic waste?” This chart shows the 
+        top ten countries that emit the most plastic waste to the ocean per 
+        capita in kilograms per year. The ten countries (in descending order) 
+        that emit the most plastic waste to the ocean: Philippines, Suriname, 
+        Trinidad and Tobago, Saint Lucia, Malaysia, Guyana, Panama, Grenada, 
+        Belize, and Jamaica. The country that emits the most waste per capita is 
+        the Philippines, with over three kilograms (3.296 kg/yr) of mismanaged 
+        plastic waste in the ocean per year. The country that emits the least 
+        per capita (in the top ten) is Jamaica, with less than one kilogram 
+        (0.792 kg/yr) of mismanaged plastic waste to the ocean per year. 
+        The ten countries (in ascending order) that emit the least plastic waste
+        emission to the ocean (excluding countries with zero emissions) are: 
+        Saudi Arabia, Jordan, Somalia, Oman, Kazakhstan, Georgia, Poland, 
+        Namibia, Bulgaria, and Australia. Saudia Arabia per capita emits 
+        0.000088 kilograms per year of plastic to the ocean; Australia per 
+        capita emits 0.0013 kilograms per year of plastic waste to the ocean."),
+      p("These charts show the top ten countries that release the most plastic 
+        waste to the ocean per capita in kilograms per year. These countries 
+        should be held accountable for their plastic waste emissions to the 
+        ocean. Policymakers in these countries should implement laws and 
+        regulations to prevent these emissions so that the environment will be 
+        cleaner and safer for everyone and marine animals. The organizations 
+        and technology designers that focus on cleaning the ocean should be 
+        brought to these countries to aid in cleaning up the plastic waste 
+        emissions in the surrounding environment and ocean. The countries that 
+        emit the most plastic should learn from the countries that emit the 
+        least (including the countries that emit zero plastic waste) and how 
+        those countries prevent their waste from entering the ocean.")
+    )
+  )
 )
