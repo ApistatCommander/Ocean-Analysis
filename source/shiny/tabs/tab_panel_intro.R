0bb8f1f6d7f4235b8af3ed6571edab43361d3411
jasminepw <jasminee.wonggg@gmail.com>
2022-12-07 20:48:48 -0800

    adding image to intro tab

--- source/shiny/tabs/tab_panel_intro.R
+++ source/shiny/tabs/tab_panel_intro.R
@@ -4,5 +4,14 @@ library(shiny)
 
 tab_panel_intro <-tabPanel(
     "Introduction",
-    p("This is the introduction.")
+    p("This is the introduction."),
+    tags$figure(
+      align = "center",
+      tags$img(
+        src = "TOC_Entangled_Turtle_by_Francis_Perez1.jpg",
+        width = 500,
+        alt = "A sea turtle entangled in a ghost net"
+      ),
+      tags$figcaption("Entangled Turtle by Francis Perez")
+    )
 )
